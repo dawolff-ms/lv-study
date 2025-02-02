7f99112ba4212c81b1efd8ca5f5f9c5304788339
Daniel Wolff <dawolff@microsoft.com>
2025-02-01 17:44:36 -0800

    implementing image provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import App from './App.tsx'
+import ImageProvider from './data-provider/ImageProvider.ts'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+const BlobStorageUrl = import.meta.env.VITE_BlobStorageUrl;
+
+const imageProvider = new ImageProvider(BlobStorageUrl);
+console.log(imageProvider.getImageList());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
