8763eec1c2378cf5d7e82bcc7d69e376c352722d
Daniel Wolff <dawolff@microsoft.com>
2025-02-02 15:49:38 -0800

    adding survey context and controller

---
+++ src/controllers/SurveyController.ts
@@ -0,0 +1,97 @@
+import ImageProvider from "../data-provider/ImageProvider";
+import Listenable from "../utils/Listenable";
+import { shuffle } from "../utils/ArrayUtils";
+
+export type ImageState = {
+  source: string;
+  hidden: boolean;
+};
+
+type InternalImageState = ImageState & {
+  start?: number;
+  delay?: number;
+};
+
+export type SurveyControllerEvent =
+  | { type: "image-state-update"; image: ImageState }
+  | { type: "survey-completed" };
+
+export default class SurveyController extends Listenable<SurveyControllerEvent> {
+  private imageProvider: ImageProvider;
+
+  private images: InternalImageState[] = [];
+  private currentIndex = -1;
+
+  private _initialization: Promise<void>;
+  public get initialization(): Promise<void> {
+    return this._initialization;
+  }
+
+  constructor(imageProvider: ImageProvider) {
+    super();
+    this.imageProvider = imageProvider;
+
+    this.initialize = this.initialize.bind(this);
+    this.getCurrentImage = this.getCurrentImage.bind(this);
+    this.acknowledge = this.acknowledge.bind(this);
+    this.skip = this.skip.bind(this);
+    this.queueNextImage = this.queueNextImage.bind(this);
+
+    this._initialization = this.initialize();
+  }
+
+  private async initialize(): Promise<void> {
+    try {
+      const sources = await this.imageProvider.getImageList().then(shuffle);
+      this.images = sources.map((source) => ({ source, hidden: true }));
+
+      this.queueNextImage();
+    } catch (error) {
+      console.error(
+        "Something went wrong while initializing the SurveyController",
+        error
+      );
+      throw error;
+    }
+  }
+
+  public getCurrentImage(): ImageState {
+    return this.images[this.currentIndex];
+  }
+
+  public acknowledge(image: ImageState): void {
+    const currentImage = this.images[this.currentIndex];
+    if (image.source !== currentImage.source) return;
+    if (currentImage.start == null) return;
+
+    const now = performance.now();
+    const duration = now - currentImage.start;
+
+    console.log(`Acknowledged image ${currentImage.source} in ${duration}ms`);
+    this.queueNextImage();
+  }
+
+  public skip(): void {
+    console.log(`Skipped image ${this.images[this.currentIndex].source}`);
+    this.queueNextImage();
+  }
+
+  private queueNextImage(): void {
+    this.currentIndex++;
+    if (this.currentIndex >= this.images.length) {
+      this.updateListeners({ type: "survey-completed" });
+      return;
+    }
+
+    const image = this.images[this.currentIndex];
+    image.hidden = true;
+    this.updateListeners({ type: "image-state-update", image: { ...image } });
+
+    image.delay = Math.floor(Math.random() * 5000) + 1000;
+    setTimeout(() => {
+      image.hidden = false;
+      image.start = performance.now();
+      this.updateListeners({ type: "image-state-update", image: { ...image } });
+    }, image.delay);
+  }
+}
