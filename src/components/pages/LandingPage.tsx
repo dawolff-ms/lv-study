41b11047f5f1e940b320eb85ccde0e223a33bec3
Daniel Wolff <dawolff@microsoft.com>
2025-03-25 22:47:10 -0700

    updating landing page content

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,10 +1,10 @@
 import {
   Body1,
   Button,
-  Divider,
   Select,
   SelectOnChangeData,
   SpinButton,
+  Subtitle1,
   Title1,
   makeStyles,
 } from "@fluentui/react-components";
@@ -37,32 +37,47 @@ export default function LandingPage() {
 
   return (
     <Flex gap={12} className={styles.container}>
-      <Title1 as="h1">Low-vision Survey</Title1>
+      <Title1 as="h1">Hello - thank you for participating in this study</Title1>
+
+      <Subtitle1 as="h2">About the survey</Subtitle1>
       <Body1>
-        Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquam inceptos at
-        diam; lectus facilisi nibh viverra. Lobortis ligula vehicula amet sem
-        quisque aliquam nibh habitant tortor. Tristique nam id aliquet quis nec
-        pharetra purus dolor. Pharetra tempor ut fermentum purus purus ac
-        porttitor. Phasellus cubilia curae quis ipsum maximus non fringilla.
+        You'll be shown a series of designs for a text box, in either light or
+        dark mode based on your selection. Each design will be shown one at a
+        time.
       </Body1>
+      <ul>
+        <li>
+          <Body1>
+            If you're able to see the text box, press the space bar on your
+            keyboard or click your mouse.
+          </Body1>
+        </li>
+        <li>
+          <Body1>If you don't see it, don't take any action.</Body1>
+        </li>
+      </ul>
       <Body1>
-        Gravida taciti metus aenean a sociosqu eleifend cubilia. Parturient
-        dolor lacus dui ac congue inceptos euismod lacinia. Porttitor
-        suspendisse mauris ut porttitor mi nisi facilisis. Natoque justo litora
-        per ut pretium feugiat proin. Suscipit montes ornare quam varius viverra
-        vehicula leo.
-      </Body1>
-      <Body1>
-        Porta massa class pellentesque auctor neque malesuada turpis cursus.
-        Turpis adipiscing neque aliquam nascetur per suspendisse consectetur
-        amet fusce. Proin litora velit vestibulum placerat interdum orci eros
-        mattis. Molestie hac turpis lacus rhoncus pretium. Fermentum curabitur
-        parturient sapien mollis ut nostra accumsan senectus.
+        There are no wrong answers. It's important for us to understand which
+        designs are or are not perceivable, so your honest response is
+        appreciated.
       </Body1>
 
-      <br />
-      <Divider />
-      <br />
+      <Subtitle1 as="h2">Rest breaks</Subtitle1>
+      <Body1>
+        You can choose how often you want to take a break while completing the
+        survey. Based on your selection, the survey will automatically pause.
+      </Body1>
+      <Body1>
+        Please feel free to close your eyes or whatever way you need to reset,
+        and take as much time as you need. Once you're ready to continue, press
+        the space bar on your keyboard or click your mouse.
+      </Body1>
+      <Body1>
+        Also if at any other point you want to take a break, please let the
+        moderator know.
+      </Body1>
+
+      <Subtitle1 as="h2">To begin the survey</Subtitle1>
 
       <label htmlFor={selectId}>Select the mode you'd like to test:</label>
       <Select
